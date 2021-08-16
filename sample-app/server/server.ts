d246c96e82a929b0a96e87d8a10106d2eade5aab
Jack V Curtis <jack.v.curtis@gmail.com>
2021-08-16 10:14:29 -0400

    whitespace

--- sample-app/server/server.ts
+++ sample-app/server/server.ts
@@ -1,32 +1,33 @@
 import path from 'path'
 import express, { Application, Request, Response } from "express"
-import {registerReactSSREngine} from "express-react-ssr-engine"
+import { registerReactSSREngine } from "express-react-ssr-engine"
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 
 export async function configureServer(): Promise<Application> {
     const app = express()
     await registerReactSSREngine(
-        app, 
-        path.join(__dirname, 'views'), 
+        app,
+        path.join(__dirname, 'views'),
         React,
         ReactDOMServer,
-        { 
-        externals: {
-            "react-bootstrap": {
-                commonjs: "react-bootstrap",
-                commonjs2: "react-bootstrap",
-                amd: "ReactBootstrap",
-                root: "ReactBootstrap"
-              }
+        {
+            externals: {
+                "react-bootstrap": {
+                    commonjs: "react-bootstrap",
+                    commonjs2: "react-bootstrap",
+                    amd: "ReactBootstrap",
+                    root: "ReactBootstrap"
+                }
+            }
         }
-    })
+    )
 
     app.get('/', (req: Request, res: Response) => {
-        res.render('landing', { props: { message: "Hello from the server"}})
+        res.render('landing', { props: { message: "Hello from the server" } })
     })
     app.get('/users', (req: Request, res: Response) => {
-        res.render('users/index', { props: {users: [{ name: 'Freddie Mercury' }, { name: 'Tupac Shakur' }] }})
+        res.render('users/index', { props: { users: [{ name: 'Freddie Mercury' }, { name: 'Tupac Shakur' }] } })
     })
 
     return app
